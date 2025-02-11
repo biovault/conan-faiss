1aa5a1f009b4d802a6b240db91ef6e062d00a190
Alexander Vieth <vieth.alexander@gmx.net>
2025-02-11 21:41:28 +0100

    Maybe now this just works now

--- conanfile.py
+++ conanfile.py
@@ -98,21 +98,20 @@ class FaissConan(ConanFile):
         if os_info.is_macos:
             proc = subprocess.run("brew --prefix libomp", shell=True, capture_output=True)
             omp_prefix_path = f"{proc.stdout.decode('UTF-8').strip()}"
+            tc.variables["OpenMP_ROOT"] = omp_prefix_path
 
-            print(f"self.settings.arch: {self.settings.arch}")
-
-            if self.settings.arch == "armv8":
-                print("ARM")
-                tc.variables["OpenMP_CXX_FLAGS"] = "-Xclang -fopenmp"
-                tc.variables["OpenMP_C_FLAG"] = "-Xclang -fopenmp"
-                tc.variables["OpenMP_CXX_LIB_NAMES"] = "libomp"
-                tc.variables["OpenMP_C_LIB_NAMES"] = "libomp"
-                tc.variables["OpenMP_CXX_INCLUDE_DIR"] = f"{omp_prefix_path}/include"
-                tc.variables["OpenMP_C_INCLUDE_DIR"] = f"{omp_prefix_path}/include"
-                tc.variables["OpenMP_libomp_LIBRARY"] = f"{omp_prefix_path}/lib/libomp.dylib"
-            else:
-               print("x86")
-               tc.variables["OpenMP_ROOT"] = omp_prefix_path
+            # if self.settings.arch == "armv8":
+            #     print("Apple armv8")
+            #     tc.variables["OpenMP_CXX_FLAGS"] = "-Xclang -fopenmp"
+            #     tc.variables["OpenMP_C_FLAG"] = "-Xclang -fopenmp"
+            #     tc.variables["OpenMP_CXX_LIB_NAMES"] = "libomp"
+            #     tc.variables["OpenMP_C_LIB_NAMES"] = "libomp"
+            #     tc.variables["OpenMP_CXX_INCLUDE_DIR"] = f"{omp_prefix_path}/include"
+            #     tc.variables["OpenMP_C_INCLUDE_DIR"] = f"{omp_prefix_path}/include"
+            #     tc.variables["OpenMP_libomp_LIBRARY"] = f"{omp_prefix_path}/lib/libomp.dylib"
+            # else:
+            #    print("Apple x86_64")
+            #    tc.variables["OpenMP_ROOT"] = omp_prefix_path
 
 
         tc.variables["CMAKE_CXX_STANDARD"] = "17"
